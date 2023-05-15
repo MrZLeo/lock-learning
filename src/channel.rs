c3add5c9c881d0772ff32b680666f5e5b7e5c0f9
Zhang Jing <mrzleo@foxmail.com>
2023-05-15 18:42:37 +0800

    relatively safe oneshot channel

--- src/channel.rs
+++ src/channel.rs
@@ -3,12 +3,13 @@ use std::{
     mem::MaybeUninit,
     sync::atomic::{
         AtomicBool,
-        Ordering::{Acquire, Release},
+        Ordering::{Acquire, Relaxed, Release},
     },
 };
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
+    in_use: AtomicBool,
     ready: AtomicBool,
 }
 
@@ -18,24 +19,72 @@ impl<T> Channel<T> {
     pub const fn new() -> Self {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()),
+            in_use: AtomicBool::new(false),
             ready: AtomicBool::new(false),
         }
     }
 
-    /// # Safety
-    /// Only call this once!
-    pub unsafe fn send(&self, message: T) {
-        (*self.message.get()).write(message);
+    /// Pnaics when trying to send more than one message.
+    pub fn send(&self, message: T) {
+        if self.in_use.swap(true, Relaxed) {
+            panic!("can't send more than one message!");
+        }
+        unsafe {
+            (*self.message.get()).write(message);
+        }
         self.ready.store(true, Release);
     }
 
     pub fn is_ready(&self) -> bool {
-        self.ready.load(Acquire)
+        // we can use Relaxed here because `receive()` use Release,
+        // and `ready` has only one order among all threads.
+        // if `is_ready()` execute before `receive()`, which is the correct usage,
+        // `receive()` must see true if `is_ready()` sees true
+        self.ready.load(Relaxed)
     }
 
-    /// # Safety
-    /// Only call this once and only after `is_ready()` returns true!
-    pub unsafe fn receive(&self) -> T {
-        (*self.message.get()).assume_init_read()
+    /// Panic if no message is available yet,
+    /// or message was already consumed.
+    ///
+    /// Tip: Use `is_ready()` to check first.
+    pub fn receive(&self) -> T {
+        if !self.ready.swap(false, Acquire) {
+            panic!("no message available!");
+        }
+        // Safety: We've just checked (and reset) the ready flag.
+        unsafe { (*self.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe {
+                self.message.get_mut().assume_init_drop();
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::thread;
+
+    use super::Channel;
+
+    #[test]
+    fn oneshot_channel() {
+        let channel = Channel::new();
+        let t = thread::current();
+        thread::scope(|s| {
+            s.spawn(|| {
+                channel.send("hello world!");
+                t.unpark();
+            });
+            while !channel.is_ready() {
+                thread::park();
+            }
+            assert_eq!(channel.receive(), "hello world!");
+        })
     }
 }
