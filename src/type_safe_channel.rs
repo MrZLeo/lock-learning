7d254302810ad76edb67ca4ae06c732f228e7a7a
Zhang Jing <mrzleo@foxmail.com>
2023-05-15 19:54:48 +0800

    type safe channel using borrow

--- src/type_safe_channel.rs
+++ src/type_safe_channel.rs
@@ -1,19 +1,29 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::{
-        atomic::{
-            AtomicBool,
-            Ordering::{Acquire, Relaxed, Release},
-        },
-        Arc,
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Relaxed, Release},
     },
 };
 
-/// Inner type, which is just the implementation detail inside the lib
-struct TypeSafeChannel<T> {
+pub struct TypeSafeChannel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
+}
+
+impl<T> TypeSafeChannel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split(&mut self) -> (Sender<T>, Receiver<T>) {
+        *self = Self::new();
+        (Sender { channel: self }, Receiver { channel: self })
+    }
 }
 
 impl<T> Drop for TypeSafeChannel<T> {
@@ -28,15 +38,15 @@ impl<T> Drop for TypeSafeChannel<T> {
 
 unsafe impl<T> Sync for TypeSafeChannel<T> where T: Send {}
 
-pub struct Sender<T> {
-    channel: Arc<TypeSafeChannel<T>>,
+pub struct Sender<'a, T> {
+    channel: &'a TypeSafeChannel<T>,
 }
 
-pub struct Receiver<T> {
-    channel: Arc<TypeSafeChannel<T>>,
+pub struct Receiver<'a, T> {
+    channel: &'a TypeSafeChannel<T>,
 }
 
-impl<T> Sender<T> {
+impl<T> Sender<'_, T> {
     pub fn send(self, message: T) {
         unsafe {
             (*self.channel.message.get()).write(message);
@@ -45,7 +55,7 @@ impl<T> Sender<T> {
     }
 }
 
-impl<T> Receiver<T> {
+impl<T> Receiver<'_, T> {
     pub fn is_ready(&self) -> bool {
         self.channel.ready.load(Relaxed)
     }
@@ -58,30 +68,17 @@ impl<T> Receiver<T> {
     }
 }
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let channel = Arc::new(TypeSafeChannel {
-        message: UnsafeCell::new(MaybeUninit::uninit()),
-        ready: AtomicBool::new(false),
-    });
-
-    (
-        Sender {
-            channel: channel.clone(),
-        },
-        Receiver { channel },
-    )
-}
-
 #[cfg(test)]
 mod test {
     use std::thread;
 
-    use super::channel;
+    use super::TypeSafeChannel;
 
     #[test]
     fn type_safe_channel() {
+        let mut channel = TypeSafeChannel::new();
         thread::scope(|s| {
-            let (sender, receiver) = channel();
+            let (sender, receiver) = channel.split();
             let t = thread::current();
             s.spawn(move || {
                 sender.send("hello world!");
