3b2fe8560772c30f55b1fc4a6c9ea736d859f5eb
Zhang Jing <mrzleo@foxmail.com>
2023-05-15 16:46:20 +0800

    usable but unsafe channel

--- src/channel.rs
+++ src/channel.rs
@@ -1,39 +1,41 @@
 use std::{
-    collections::VecDeque,
-    sync::{Condvar, Mutex},
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Release},
+    },
 };
 
 pub struct Channel<T> {
-    queue: Mutex<VecDeque<T>>,
-    item_ready: Condvar,
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
 }
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Channel<T> {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
-            queue: Mutex::new(VecDeque::new()),
-            item_ready: Condvar::new(),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
         }
     }
 
-    pub fn send(&self, message: T) {
-        self.queue.lock().unwrap().push_back(message);
-        self.item_ready.notify_one();
+    /// # Safety
+    /// Only call this once!
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release);
     }
 
-    pub fn receive(&self) -> T {
-        let mut queue = self.queue.lock().unwrap();
-        loop {
-            if let Some(message) = queue.pop_front() {
-                return message;
-            }
-            queue = self.item_ready.wait(queue).unwrap();
-        }
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Acquire)
     }
-}
 
-impl<T> Default for Channel<T> {
-    fn default() -> Self {
-        Self::new()
+    /// # Safety
+    /// Only call this once and only after `is_ready()` returns true!
+    pub unsafe fn receive(&self) -> T {
+        (*self.message.get()).assume_init_read()
     }
 }
