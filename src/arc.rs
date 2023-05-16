8a62e86437adc3409887cd024edd7773a2b57f95
Zhang Jing <mrzleo@foxmail.com>
2023-05-16 13:52:54 +0800

    mutation for Arc

--- src/arc.rs
+++ src/arc.rs
@@ -32,6 +32,35 @@ impl<T> Arc<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe { self.ptr.as_ref() }
     }
+
+    /// This function must be used like:
+    ///
+    /// ```
+    /// Arc::get_mut(&mut a);
+    /// ```
+    ///
+    /// since `T` could be types that implement `Deref`, which
+    /// will cause ambiguity in dereferencing `Arc` or `T` if we allow:
+    ///
+    /// ```
+    /// a.get_mut(); // dereference T or Arc?
+    /// ```
+    ///
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        // Make sure that every operations before the Drop's `Release`
+        // store **happens before** this load.
+        //
+        // We just need to make sure that Ordering is `Acquire`
+        // when `ref_count` == 1, so `fence()` is better option.
+        if arc.data().ref_count.load(Relaxed) == 1 {
+            fence(Acquire);
+            // Safety: Nothing else can access the data, since
+            // there's only one Arc, to which we have exclusive access.
+            unsafe { Some(&mut arc.ptr.as_mut().data) }
+        } else {
+            None
+        }
+    }
 }
 
 impl<T> Deref for Arc<T> {
