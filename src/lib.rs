ebd2f0161d41422fa4a7a9d0eb40bc2fcf3d8fc1
Zhang Jing <mrzleo@foxmail.com>
2023-05-21 16:04:22 +0800

    RwLock

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod arc;
 pub mod channel;
 pub mod condition_variable;
 pub mod mutex;
+pub mod read_write_lock;
 pub mod spin;
 pub mod state_machine_channel;
 pub mod type_safe_channel;
---
+++ src/read_write_lock.rs
@@ -0,0 +1,94 @@
+use std::{
+    assert_ne,
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::{
+        AtomicU32,
+        Ordering::{Acquire, Relaxed, Release},
+    },
+};
+
+use atomic_wait::{wait, wake_all, wake_one};
+
+pub struct RwLock<T> {
+    // The number of readers, or u32::MAX if write-locked
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for RwLock<T> where T: Send + Sync {}
+
+pub struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.rwlock.state.fetch_sub(1, Release) == 1 {
+            wake_one(&self.rwlock.state);
+        }
+    }
+}
+
+pub struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.store(0, Release);
+        wake_all(&self.rwlock.state);
+    }
+}
+
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut s = self.state.load(Relaxed);
+        loop {
+            if s < u32::MAX {
+                assert_ne!(s, u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(s, s + 1, Acquire, Relaxed) {
+                    Ok(_) => return ReadGuard { rwlock: self },
+                    Err(e) => s = e,
+                }
+            } else {
+                wait(&self.state, u32::MAX);
+                s = self.state.load(Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        while let Err(s) = self.state.compare_exchange(0, u32::MAX, Acquire, Relaxed) {
+            wait(&self.state, s);
+        }
+        WriteGuard { rwlock: self }
+    }
+}
